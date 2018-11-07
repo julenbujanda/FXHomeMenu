d91ab8f1f5789361d0dab9d0fa4fe3b5b89059e6
julenb <21701719@live.uem.es>
2018-11-07 19:24:53 +0100

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -9,7 +12,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-
+        Parent root = FXMLLoader.load(getClass().getResource("view/Home.fxml"));
+        primaryStage.setTitle("Home");
+        primaryStage.setScene(new Scene(root, 900, 500));
+        primaryStage.show();
     }
 
 }
